2628feccb8c6e665120ea30bff61da8da4522949
CallumCQ <callum.macpherson@cambridgequantum.com>
2022-07-13 15:35:07 +0100

    updaate js

--- embed_notebook.js
+++ embed_notebook.js
@@ -1,7 +1,7 @@
 <script>
 (async () => {
   const res = await fetch(
-    "https://api.github.com/repos/calmacCQ/tket_blog/contents/Blog_draft.html"
+    "https://api.github.com/repos/calmacCQ/tket_blog/contents/blog1_intro_to_qc.html"
   );
   const data = await res.json();
   document.querySelector("iframe").src = `data:text/html;base64,${data["content"]}`;
